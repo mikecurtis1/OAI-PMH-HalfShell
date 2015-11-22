9aa0aba07b0ae445eaf744a9f22f3bfca6ab7fe5
Mike Curtis <mikecurtis1@gmail.com>
2015-11-22 14:23:30 -0500

    added initial mysql database and query for model, changed format of URN in readme.md

---
+++ Models/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+SELECT 
+    CONCAT('urn:example.com/half_shell:books:', `identifier`) AS `identifier`, 
+    `modified`, 
+    IF(`status` = 0, 'deleted', 'active') AS `header_status`, 
+    `author`, 
+    `year`, 
+    `title`, 
+    `description`, 
+    `isbn`, 
+    `language_ISO639-3`, 
+    CONCAT(`root_set`, ':', `sub_set`) AS `setSpec`
+FROM `books` 
+ORDER BY `modified` DESC
+*/
