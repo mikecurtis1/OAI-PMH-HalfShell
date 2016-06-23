a754bb5cc6ab22c5241b6af36ed1a75c25af38e7
Mike Curtis <mikecurtis1@gmail.com>
2016-06-23 15:49:13 -0400

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,9 @@
 <?php 
 
+//NOTE: do not display errors in production
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
-error_reporting(-1);
+error_reporting(E_ALL);
 
 require_once dirname(__FILE__) . '/View/AbstractView.php';
 require_once dirname(__FILE__) . '/View/View.php';
